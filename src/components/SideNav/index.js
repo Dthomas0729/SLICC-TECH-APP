e33320989400ec5aed45a3af9eae107c74bc91d8
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-08-11 23:59:41 -0400

    Start SLICC app, ADD sidenav background, ADD components folder and pages

---
+++ src/components/SideNav/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function SideNav() {
+    return (
+        <nav className='sideNav' style={{ width: '25%', paddingTop: '20px' }}>
+            <Link />
+            <Link />
+            <Link />
+            <Link />
+        </nav>
+        
+    )
+}
+
+export default SideNav
