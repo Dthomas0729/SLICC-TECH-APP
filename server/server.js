15ad36f1a9765f44a0b6484ceb4a86556db16921
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-09-01 09:05:03 -0400

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
+// ========= EXTERNAL MODULES
 const express = require('express');
-const app = express();
 const path = require("path");
+const cors = require("cors");
+
+// ========= INTERNAL MODULES
 
 
+// ========= INSTANCE MODULES
+const app = express();
+
+// ========= CONIGURATION
+
+// ========= MIDLLEWARE
+app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
+app.use(cors());
+app.use(express.json());
 
+// ======= REACT FULL STACK MAGIC MIDDLEWARE
 app.use((req, res, next) => {
-    console.log(req.headers);
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "build", "../public/index.html"));
 });
 
 // start express server on port 5000
 app.listen(5000, () => {
-    console.log("server started on port 5000");
+    console.log("SLICC Developers WE ARE LIVE!: http://localhost:5000");
 });
