a9cae89916f48aced6eb47cfbcfa60f9b5e202c9
Dominique Thomas <63202540+Dthomas0729@users.noreply.github.com>
2021-10-11 19:06:18 -0400

    ADD styling to Welcome component

--- src/pages/Home/Welcome/index.js
+++ src/pages/Home/Welcome/index.js
@@ -9,7 +9,17 @@ const Welcome = () => {
             style={{ backgroundImage: `url(${background})` }}
             className='welcome-container'
         >
-            <h1>SLICC TECH</h1>
+            <h1 className='welcome-header'>
+                Web Applications & Software for Small Businesses</h1>
+
+            <p className='welcome-body'>We build full stack web applications for small and startup businesses to make it cost effective for them to own software that solves company problems.</p>
+
+            <div className='welcome-actions'>
+
+                    <button className='getaquote'>Get a Quote</button>
+                    <button className='viewservices'>View Services</button>
+
+            </div>
         </div>
     )
 }
